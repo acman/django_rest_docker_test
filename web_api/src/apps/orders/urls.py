2251dedf0433aaba778908d990e78075c0b44b4c
acman <webasman@gmail.com>
2018-06-03 21:09:51 +0300

    add date range filter

--- web_api/src/apps/orders/urls.py
+++ web_api/src/apps/orders/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('orders', views.OrderViewSet)
+router.register('orders', views.OrderViewSet, 'orders')
 
 
 urlpatterns = [
--- web_api/src/apps/orders/views.py
+++ web_api/src/apps/orders/views.py
@@ -6,13 +6,24 @@ from .serializers import OrderListSerializer, OrderCreateUpdateSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
     serializers = {
         'default': OrderListSerializer,
         'create': OrderCreateUpdateSerializer,
         'update': OrderCreateUpdateSerializer,
     }
     permission_classes = (AnonCreateAndRetrieveUpdateDeleteOwnerOrStaffOnly, )
+
+    def _get_filters(self):
+        filters = {}
+        created_date_range = self.request.GET.get('created_date__range')
+        if created_date_range:
+            date_min, date_max = created_date_range.split(',')
+            filters['created_date__range'] = [date_min[1:], date_max[:-1]]
+
+        return filters
+
+    def get_queryset(self):
+        return Order.objects.filter(**self._get_filters())
 
     def get_serializer_class(self):
         """
